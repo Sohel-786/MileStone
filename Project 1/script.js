3fc70f7340a9b419b16d7c82d15f3c8ea607250e
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-08-11 20:42:07 +0530

    flexing

--- Project 1/script.js
+++ Project 1/script.js
@@ -20,9 +20,46 @@ function handleBlog(){
                         form.blog.value
                         );
                         
-    let blogs = localStorage.getItem('All_Blogs');
+    let blogs_data = localStorage.getItem('All_Blogs');
 
-    if(blogs){
-        blogs.
+    if(blogs_data){
+
+        blogs_data = JSON.parse(blogs_data);
+        
+    }else{
+        blogs_data = [];
     }
+    
+    blogs_data.push(blog);
+    
+    localStorage.setItem('All_Blogs', JSON.stringify(blogs_data))
+
+    window.location.reload();
 }
+
+function handleBlogList(){
+
+    let blogs_data = localStorage.getItem('All_Blogs');
+
+    if(!blogs_data){
+        return;
+    }
+
+    blogs_data = JSON.parse(blogs_data);
+    
+    blogs_data = blogs_data.map( (el) => {
+
+        return `<div><div><img src=${el.url}/></div><h1>${el.title}</h1><p>${el.desc}</p><button>Read</button></div>`
+    });
+
+    // console.log(blogs_data);
+
+    let container = document.querySelector('#container');
+
+    blogs_data.forEach(el => {
+        container.innerHTML = el;
+    });
+
+}
+
+handleBlogList();
