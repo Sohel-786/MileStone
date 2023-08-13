5f86af027e3b8b09e2db41da6d19b746332faf97
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-08-13 20:52:12 +0530

    Practice

---
+++ Project 3/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+        <div>
+          <input type="text" placeholder='Enter Todo'/>
+          <button>Add</button>
+        </div>
+    </div>
+  )
+}
+
+export default App
